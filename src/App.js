1fb20dab4e6059c7ff0d9dff0e5524e2e0a02fb7
Justin <fernbaughj@gmail.com>
2022-12-13 17:44:11 -0800

    Centered login button

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ import Button from 'react-bootstrap/Button';
 
 import './App.css';
 
-const ExampleToast = ({ children }) => {
+// const ExampleToast = ({ children }) => {
+//   const [show, toggleShow] = useState(true);
+
+//   return (
+//     <>
+//       {!show && <Button onClick={() => toggleShow(true)}>Login</Button>}
+//       <Toast show={show} onClose={() => toggleShow(false)}>
+//         <Toast.Header>
+//           <strong className="mr-auto">React-Bootstrap</strong>
+//         </Toast.Header>
+//         <Toast.Body>{children}</Toast.Body>
+//       </Toast>
+//     </>
+//   );
+// };
+
+const AuthRedirect = ({ children }) => {
   const [show, toggleShow] = useState(true);
 
   return (
     <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      {!show && <Button onClick={() => toggleShow(true)}>Login</Button>}
       <Toast show={show} onClose={() => toggleShow(false)}>
         <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
+          <strong className="mr-auto">Redirect</strong>
         </Toast.Header>
         <Toast.Body>{children}</Toast.Body>
       </Toast>
@@ -25,13 +41,15 @@ const ExampleToast = ({ children }) => {
 const App = () => (
   <Container className="p-3">
     <Container className="p-5 mb-4 bg-light rounded-3">
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
+      <h1 className="header">Spotify Manager</h1>
+      <div class="login h-100 d-flex align-items-center justify-content-center">
+        <AuthRedirect>
+          We now have Toasts
+          <span role="img" aria-label="tada">
+            🎉
+          </span>
+        </AuthRedirect>
+      </div>
     </Container>
   </Container>
 );
